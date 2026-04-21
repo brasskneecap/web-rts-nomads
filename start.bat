7dcb4a886a4245b71b437cc8cfffff6ff62cb463
brasskneecap <codyanderson91@gmail.com>
2026-04-21 00:20:34 -0600

    corrected start.bat to work across machines

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-start "Frontend" /d "%USERPROFILE%\web-rts-nomads\client\src\game-portal" cmd /k dev.bat
-start "Backend" /d "%USERPROFILE%\web-rts-nomads\server" cmd /k dev.bat
+start "Frontend" /d "client\src\game-portal" cmd /k dev.bat
+start "Backend" /d "server" cmd /k dev.bat
 echo Started: frontend at http://localhost:5173  ^|  backend at http://localhost:8080
